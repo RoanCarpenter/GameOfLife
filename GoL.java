3b2dce241cb193223a20dd1cfe8f524fc6c0ec6c
Roan Carpenter <roan.carpenter@mrjaffesclass.com>
2023-01-24 14:21:13 -0800

    peppe

---
+++ GoL.java
@@ -0,0 +1,23 @@
+
+public class GoL{
+
+    public static void main(String args[]){
+        int M = 10;
+        int N = 10;
+
+        int grid [][] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            };
+        System.out.println("Original Life");
+    }
+
+    public static void nextGeneration(int grid [][], int M, intN)
+}
